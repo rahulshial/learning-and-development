b44c1bd0d025f2350f08c2282546b46edc69d7be
Rahul Shial <rahul.shial@gmail.com>
2025-02-05 10:52:36 -0700

    interger class

--- monkey_patching.rb
+++ monkey_patching.rb
@@ -14,3 +14,23 @@ class Person
 end
 
 person.hello # this will print "Hello"
+
+#Active support allows us to add methods to the Integer class
+#But this is very dangerous because it can cause conflicts with other libraries
+
+class Integer
+  def hours
+    60 * minutes
+  end
+
+  def minutes
+    self * 60
+  end
+
+  def days
+    24 * hours
+  end
+end
+
+puts 2.hours
+puts 3.days
